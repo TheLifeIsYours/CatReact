f70e5a85b05ac74210934979cef6443f7e03eacb
thelifeisyours <7der54@gmail.com>
2020-09-16 19:36:00 +0200

    Updates

---
+++ precache-manifest.95c25cee1b1f86803dbcbc7b6499f9f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efdc91ce51dc69d369bf2955a1f8a90c",
+    "url": "/CatReact/index.html"
+  },
+  {
+    "revision": "b6431a98a49333f8a461",
+    "url": "/CatReact/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cb9cf122016dd10080b2",
+    "url": "/CatReact/static/js/2.747c7f1a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CatReact/static/js/2.747c7f1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6431a98a49333f8a461",
+    "url": "/CatReact/static/js/main.0c292d5d.chunk.js"
+  },
+  {
+    "revision": "4b3a5ca52599a183688a",
+    "url": "/CatReact/static/js/runtime-main.0b9fd28f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b14a2e02fdd25ca3ca530301fedca184.js"
+  "/CatReact/precache-manifest.95c25cee1b1f86803dbcbc7b6499f9f6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/CatReact/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
