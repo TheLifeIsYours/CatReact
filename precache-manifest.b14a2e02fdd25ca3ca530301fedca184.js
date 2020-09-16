5f139278dc2253a6d90eadf74102280a719b8e61
thelifeisyours <7der54@gmail.com>
2020-09-16 19:33:09 +0200

    Updates

---
+++ precache-manifest.b14a2e02fdd25ca3ca530301fedca184.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed650e0d8f548b694fe434504d8ede21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a874ff9739232e2c0f8",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cb9cf122016dd10080b2",
+    "url": "/static/js/2.747c7f1a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.747c7f1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a874ff9739232e2c0f8",
+    "url": "/static/js/main.0ec1ffe0.chunk.js"
+  },
+  {
+    "revision": "be786bbc83772e6dca26",
+    "url": "/static/js/runtime-main.2887a2af.js"
+  }
+]);
